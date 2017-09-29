6059bc1d952ea94a858cdcfec8c34f393d5b2b5a
asundark <ajaysundar.k@gmail.com>
2017-09-29 18:38:35 -0500

    Vector addition in GPU

---
+++ README.md
@@ -0,0 +1,1 @@
+# CUDA - Parallel Programming on Nvidia GPUs
---
+++ vector_addition/vectoradd_kernel.cu
@@ -0,0 +1,22 @@
+/* Vector Addition: C = A + B.
+ * Device code.
+ */
+
+#ifndef _VECTORADD_KERNEL_H_
+#define _VECTORADD_KERNEL_H_
+
+#include <stdio.h>
+#include "vectoradd.h"
+
+// Vector addition kernel thread specification
+__global__ void VectorAddKernel(Vector A, Vector B, Vector C)
+{
+	//Add the two vectors
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	if (i<A.length)
+	{
+		C.elements[i] = A.elements[i] + B.elements[i];		
+	}
+}
+
+#endif // #ifndef _VECTORADD_KERNEL_H_
